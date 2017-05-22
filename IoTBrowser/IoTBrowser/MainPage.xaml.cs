f19f522106c252a0346df85d1da8b8aa6c794bbe
Chris Grabosky <chris@grabosky.net>
2017-05-22 09:18:28 -0400

    Tab support

--- IoTBrowser/IoTBrowser/MainPage.xaml.cs
+++ IoTBrowser/IoTBrowser/MainPage.xaml.cs
@@ -5,6 +5,10 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Core;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Windows.UI.Xaml.Data;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -15,12 +19,18 @@ namespace IoTBrowser
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int _selectedTab = 0;
+        OCTab _octab = new OCTab();
+
         public MainPage()
         {
             this.InitializeComponent();
             webView.NavigationStarting += WebView_NavigationStarting;
             webView.NavigationCompleted += WebView_NavigationCompleted;
             DoWebNavigate();
+
+            lv_tabs.ItemsSource = _octab;
+            lv_tabs.SelectedIndex = 0;
         }
 
         private void WebView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
@@ -28,6 +38,15 @@ namespace IoTBrowser
             prog.Visibility = Visibility.Collapsed;
             prog.IsActive = false;
             Go_Web.Visibility = Visibility.Visible;
+
+            if (webView.DocumentTitle.Length > 24)
+            {
+                _octab[_selectedTab].Name = webView.DocumentTitle.Substring(0, 22) + "...";
+            }
+            else
+            {
+                _octab[_selectedTab].Name = webView.DocumentTitle;
+            }
         }
 
         private void WebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
@@ -98,6 +117,7 @@ namespace IoTBrowser
                     {
                         Web_Address.Text = "http://" + Web_Address.Text;
                         webView.Navigate(new Uri(Web_Address.Text));
+                        _octab[_selectedTab].Url = Web_Address.Text;
                     }
                 }
                 else
@@ -133,5 +153,53 @@ namespace IoTBrowser
         {
             Web_Address.Text = webView.Source.ToString();
         }
+
+        private void lv_tabs_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Tab t = (Tab)e.ClickedItem;
+            _selectedTab = t.Index;
+
+            Web_Address.Text = t.Url;
+            DoWebNavigate();
+        }
     }
+}
+
+public class OCTab : ObservableCollection<Tab>
+{
+    public OCTab() : base()
+    {
+        Add(new Tab("Bing", "http://bing.com", 0));
+        Add(new Tab("New Tab", "http://bing.com", 1));
+        Add(new Tab("New Tab", "http://bing.com", 2));
+        Add(new Tab("New Tab", "http://bing.com", 3));
+        Add(new Tab("New Tab", "http://bing.com", 4));
+    }
+    
+}
+
+public class Tab : INotifyPropertyChanged
+{
+    private string _name;
+    public string Name { get { return this._name; } set { this._name = value; NotifyPropertyChanged("Name"); } }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void NotifyPropertyChanged(string v)
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(v));
+    }
+
+    public string Url { get; set; }
+    public int Index { get; set; }
+
+    public Tab(string name, string url, int index)
+    {
+        this.Name = name;
+        this.Url = url;
+        this.Index = index;
+    }
+
+
 }
